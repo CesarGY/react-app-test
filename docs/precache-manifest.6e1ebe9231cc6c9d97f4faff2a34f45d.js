454d81419a6412fea0a5e8b01d11a55330fc97bf
Cesar <cesar.glez235@gmail.com>
2020-07-05 22:42:51 -0500

    Docs

---
+++ docs/precache-manifest.6e1ebe9231cc6c9d97f4faff2a34f45d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d11d57ac5133d5f39aa13fea48a5b07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d4f25e31b64bdcc67c",
+    "url": "/static/css/main.fc5379b8.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d4f25e31b64bdcc67c",
+    "url": "/static/js/main.e71482b6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
